98959fcc3587352a53db3ca2c44df3549c027cd8
syymza <syymza@gmail.com>
2015-03-28 00:25:45 +0000

    Fetch movies from Rotten Tomatoes and render the first one

--- index.ios.js
+++ index.ios.js
@@ -11,28 +11,69 @@ var {
     StyleSheet,
     Text,
     View,
-} = React;
+    } = React;
 
-var MOCKED_MOVIES_DATA = [
-    {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
-];
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
 
 var AwesomeProject = React.createClass({
-  render: function() {
-      var movie = MOCKED_MOVIES_DATA[0];
-    return (
-        <View style={styles.container}>
-            <Image
-                source={{uri: movie.posters.thumbnail}}
-                style={styles.thumbnail}
-            />
-            <View style={styles.rightContainer}>
-                <Text style={styles.title}>{movie.title}</Text>
-                <Text style={styles.year}>{movie.year}</Text>
+    getInitialState: function () {
+        return {
+            movies: null
+        };
+    },
+
+    componentDidMount: function () {
+        this.fetchData();
+    },
+
+    render: function () {
+        if (!this.state.movies) {
+            return this.renderLoadingView();
+        }
+
+        var movie = this.state.movies[0];
+        return this.renderMovie(movie);
+    },
+
+    renderLoadingView: function () {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading movies...
+                </Text>
             </View>
-        </View>
-    );
-  }
+        );
+    },
+
+    renderMovie: function (movie) {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
+    },
+
+    fetchData: function () {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies
+                });
+            })
+            .done();
+    }
 });
 
 var styles = StyleSheet.create({
